38107358d17c5c41646750382b4716490929bb2d
scgeier <scgeier@gmail.com>
2016-04-21 10:42:11 -0400

    Added an sortable table using Angular

--- public/js/chart.js
+++ public/js/chart.js
@@ -54,22 +54,22 @@ svg.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis)
     .append("text")
-      .attr("class", "label")
+      .attr("class", "x label")
       .attr("x", width)
       .attr("y", -6)
-      //.style("text-anchor", "end")
-      //.text(function(d){ return d.agriculture; });
+      .style("text-anchor", "end")
+      .text(function(d){ return setXAxisText();});
 
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
     .append("text")
-      .attr("class", "label")
+      .attr("class", "y label")
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
       .attr("dy", ".71em")
-      //.style("text-anchor", "end")
-      //.text(function(d){ return d.perCapitaGdp; });
+      .style("text-anchor", "end")
+      .text(function(d){ return setYAxisText();});
 
  svg.selectAll(".dot")
       .data(data)
@@ -80,27 +80,24 @@ svg.append("g")
       .attr("cy", function(d) { console.log(d[selectedYAxis]); return y(d[selectedYAxis]); })
       .style("fill", function(d) { return color(d.population); })
       .on("mouseenter", function(d){
-            if (d.background !== undefined) {
+        //if (d.background !== undefined) {//
                   
-        d3.select("#summary")
          tooltip.transition()
           .delay(200)
           .attr("class", "summary")
           .style("opacity", 1)
-          .style("left", "50%")
+          .style("left", "70%")
           .style("bottom", "45%")
   
-          
-          tooltip.html("<h3>" + d.id + "</h3>"
-                       + d.background)
-            }
+        tooltip.html("<h3>" + d.name + "</h3>")
+        
+        console.log(d.name);
       })
       
-    //Remove author bio on mouseleave//
+    //Remove nation info on mouseleave//
       .on("mouseleave", function(d){
-            d3.select("#summary")
-           .style("opacity", 1)
          tooltip.transition().style("opacity", 0)
+         console.log("mouseout");
       });
 
   
@@ -111,6 +108,25 @@ svg.append("g")
                         update();
      }); //close selected X function
   
+  function setXAxisText(d) {
+    if (selectedXAxis  == "obesity") {
+        return "obesity rates";
+    }else if(selectedXAxis  == "healthExpenditures"){
+        return "Percent GPD Spent on Healthcare";
+    }else{
+        return "whah?";
+    }
+  };
+  
+    function setYAxisText(d) {
+    if (selectedYAxis  == "obesity") {
+        return "obesity rates";
+    }else if(selectedYAxis  == "healthExpenditures"){
+        return "Percent GPD Spent on Healthcare";
+    }else{
+        return "whah?";
+    }
+  };
 
   function update() {
     
@@ -122,8 +138,16 @@ svg.append("g")
         
      svg.selectAll("g.y.axis")
         .call(yAxis);
+     
+     svg.selectAll(".x.label")
+        .transition()
+        .text(function(d){return setXAxisText();})
     
-    svg.selectAll(".dot")
+     svg.selectAll(".y.label")
+        .transition()
+        .text(function(d){return setYAxisText();})
+        
+     svg.selectAll(".dot")
       .transition()
       .duration(2000)
       .ease("bounce")
--- public/js/nations.js
+++ public/js/nations.js
@@ -3,7 +3,16 @@ var app = angular.module('nations', []);
 
 app.controller('NationsController', ['$http', function($http) {
 
+    var sortProperty = null;
     this.nations = [];
+  
+    this.sortProperty = "name";
+    this.reverseSort = false;
+    this.sort = function(prop){
+                  this.sortProperty = prop;
+                  this.reverseSort = !this.reverseSort;
+                };
+    
     var _this = this;
 
     $http.get('js/nations.json')
@@ -11,10 +20,13 @@ app.controller('NationsController', ['$http', function($http) {
             console.log(data);
             console.log(this);
             _this.nations = data;
+            
+
         })
         .error(function(msg) {
             console.log("This request failed.\n" + msg);
         });
+
 
     
 }]);
