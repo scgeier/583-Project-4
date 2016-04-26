0b32bec34bf077da8e33874c7b31f023a45c563e
scgeier <scgeier@gmail.com>
2016-04-26 17:38:42 -0400

    Finished the mouseover apppendix entries for the rankings table

--- public/js/table-headers.js
+++ public/js/table-headers.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
          $('#appendix-population').toggle(200);
          $('#rankings-intro-section').toggle(200);
     });
- 
   $("#population-info").mouseout(function(){
          $('#appendix-population').toggle(200);
          $('#rankings-intro-section').toggle(200);
@@ -15,18 +14,134 @@ $(document).ready(function(){
          $('#appendix-pop-growth').toggle(200);
          $('#rankings-intro-section').toggle(200);
             });
- 
   $("#pop-growth-info").mouseout(function(){
          $('#appendix-pop-growth').toggle(200);
          $('#rankings-intro-section').toggle(200);
             });
+  
    $("#migration-info").mouseover(function(){
          $('#appendix-migration').toggle(200);
          $('#rankings-intro-section').toggle(200);
             });
- 
   $("#migration-info").mouseout(function(){
          $('#appendix-migration').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  
+  $("#infant-info").mouseover(function(){
+         $('#appendix-infant').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#infant-info").mouseout(function(){
+         $('#appendix-infant').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  
+  $("#life-info").mouseover(function(){
+         $('#appendix-life').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#life-info").mouseout(function(){
+         $('#appendix-life').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  
+  $("#obesity-info").mouseover(function(){
+         $('#appendix-obesity').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#obesity-info").mouseout(function(){
+         $('#appendix-obesity').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  
+  $("#GDP-info").mouseover(function(){
+         $('#appendix-GDP').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#GDP-info").mouseout(function(){
+         $('#appendix-GDP').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  
+  $("#unemployment-info").mouseover(function(){
+         $('#appendix-unemployment').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#unemployment-info").mouseout(function(){
+         $('#appendix-unemployment').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  
+  $("#budget-info").mouseover(function(){
+         $('#appendix-budget').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#budget-info").mouseout(function(){
+         $('#appendix-budget').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+    
+  $("#debt-info").mouseover(function(){
+         $('#appendix-debt').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#debt-info").mouseout(function(){
+         $('#appendix-debt').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+ 
+  $("#education-info").mouseover(function(){
+         $('#appendix-education').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#education-info").mouseout(function(){
+         $('#appendix-education').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  
+  $("#military-info").mouseover(function(){
+         $('#appendix-military').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#military-info").mouseout(function(){
+         $('#appendix-military').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  
+  $("#area-info").mouseover(function(){
+         $('#appendix-area').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#area-info").mouseout(function(){
+         $('#appendix-area').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  
+  $("#fossil-info").mouseover(function(){
+         $('#appendix-fossil').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#fossil-info").mouseout(function(){
+         $('#appendix-fossil').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  
+  $("#airports-info").mouseover(function(){
+         $('#appendix-airports').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#airports-info").mouseout(function(){
+         $('#appendix-airports').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  
+  $("#internet-info").mouseover(function(){
+         $('#appendix-internet').toggle(200);
+         $('#rankings-intro-section').toggle(200);
+            });
+  $("#internet-info").mouseout(function(){
+         $('#appendix-internet').toggle(200);
          $('#rankings-intro-section').toggle(200);
             });
 });
